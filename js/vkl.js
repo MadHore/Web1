52fae3bf59fbeccc8121a38ffbd5e3f26cd1ca8f
MadHorse <puchok98@gmail.com>
2018-01-31 03:26:54 +0300

    Initial commit

---
+++ js/vkl.js
@@ -0,0 +1,18 @@
+var img = new Image();
+img.src = "js/pr.jpg";
+img.width = 100;
+img.height = 100;
+img.name = "spant";
+function SwitchText(my_text) {
+    if (my_text == 'kv') {
+        document.getElementById('text').innerHTML = '4';
+    }
+    if (my_text == 'opis') {
+        document.getElementById('text').innerHTML = '4000 Mhz';
+    }
+    if (my_text == 'mod') 
+        document.getElementById('text').innerHTML = 'Intel Core i7-6700K';
+    }
+
+
+
